480bb39bc4a85bfc17b09b54a6eb7cc4d1f3df0c
sumitpal29 <sumit@fusioncharts.com>
2020-03-18 16:20:19 +0530

    initial page setup and testing

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const homepage = () => {
+    // returns some jsx code which is going to display in the homepage
+    return (
+        <div className="contact-page-container">
+            <div className="contact-page-header">
+                <h1>Contact Me</h1>
+                <p className="body">How to contact</p>
+            </div>
+        </div>
+    );
+}
+
+export default homepage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const homepage = () => {
+    // returns some jsx code which is going to display in the homepage
+    return (
+        <div className="homepage-container">
+            <div className="homepage-header">
+                <h1>Header Compoment will load here</h1>
+                <p className="body">Some initial content</p>
+            </div>
+        </div>
+    );
+}
+
+export default homepage;
