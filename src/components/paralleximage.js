1f5d95ecbf989d1246107b9382e2899f817852c1
sumitpal29 <sumit@fusioncharts.com>
2020-03-19 18:21:56 +0530

    sections added contact form customised

--- src/components/paralleximage.js
+++ src/components/paralleximage.js
@@ -1,17 +1,16 @@
 import React from "react"
-import { Parallax } from 'react-scroll-parallax'
-
+import { Parallax } from "react-scroll-parallax"
 
 class ParallaxImage extends React.Component {
-    imageUrl = this.props.imageName || `percentages.svg`
+  imageUrl = this.props.imageName || `percentages.svg`
 
-    render () {
-        return (
-            <Parallax className="parallex-image-class" x={[0, 20]} tagOuter="figure">
-                <img src={`../../${this.imageUrl}`} alt={this.props.imageAlt} />
-            </Parallax>
-        )
-    }
+  render() {
+    return <img src={`../../${this.imageUrl}`} alt={this.props.imageAlt} />
+  }
 }
 
-export default ParallaxImage;
+/* <Parallax className="parallex-image-class" x={[0, 20]} tagOuter="figure">
+                <img src={`../../${this.imageUrl}`} alt={this.props.imageAlt} />
+            </Parallax> */
+
+export default ParallaxImage
--- src/components/section.js
+++ src/components/section.js
@@ -2,6 +2,9 @@ import React from "react"
 import ParallaxImage from "./paralleximage"
 
 class section extends React.Component {
+  downloadResume() {
+    console.log("called")
+  }
   render() {
     const data = this.props.sectionData
 
@@ -9,7 +12,7 @@ class section extends React.Component {
       h2: (_data, k) => <h2 key={k}>{_data}</h2>,
       h3: (_data, k) => <h3 key={k}>{_data}</h3>,
       h4: (_data, k) => <h4 key={k}>{_data}</h4>,
-      h5: (_data, k)=> <h5 key={k}>{_data}</h5>,
+      h5: (_data, k) => <h5 key={k}>{_data}</h5>,
       description: (_data, k) => <p key={k}>{_data}</p>,
       list: (_data, k) => (
         <ul key={k}>
@@ -18,8 +21,22 @@ class section extends React.Component {
           })}
         </ul>
       ),
+      button: (_data, k) => (
+        <button
+          onClick={this[_data.action]}
+          className={`btn ${_data.type}`}
+          key={k}
+        >
+          {_data.text}
+        </button>
+      ),
     }
-    const getKeys = _obj => typeof _obj === "object" ? Object.keys(_obj) : []
+    const getKeys = _obj => (typeof _obj === "object" ? Object.keys(_obj) : [])
+    const genrateKey = id =>
+      Date.now().toString(36) +
+      Math.random()
+        .toString(36)
+        .substr(2, 10)+`_${id}`
     const constructDOM = _datum => {
       const items = []
       const getDOMItems = section => {
@@ -31,17 +48,22 @@ class section extends React.Component {
             allSections.forEach(_section => {
               items.concat(getDOMItems(_section))
             })
-          } else blocks[i] && items.push(blocks[i](section[i],j+Math.ceil(Math.random()*1000)))
+          } else blocks[i] && items.push(blocks[i](section[i], genrateKey(j)))
         })
       }
 
       getDOMItems(_datum)
-      console.log("Printing from parent", _datum)
       return items
     }
 
     return (
-      <section id={data.h2.toLowerCase().split(' ').join('_')} className={this.props.classData || ''}>
+      <section
+        id={data.h2
+          .toLowerCase()
+          .split(" ")
+          .join("_")}
+        className={this.props.classData || ""}
+      >
         <div className="container content">
           <div className="content-box">{constructDOM(data)}</div>
           <div className="content-image">
