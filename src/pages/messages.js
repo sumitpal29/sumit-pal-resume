c6e84a3aaa222f968d8aa86f486300c741cb023e
sumitpal29 <sumit@fusioncharts.com>
2020-03-21 14:44:45 +0530

    firebase setup done with context

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -1,26 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react"
 
-import Layout from '../components/authLayout';
+import Layout from "../components/authLayout"
 import { useFirebase } from "../firebase/firebase-context"
 
-const LandingPage =  () => {
+const LandingPage = () => {
   const [user, setUser] = useState([])
 
-  useFirebase( firebase => {
+  useFirebase(firebase => {
     const db = firebase.firestore()
-    db.collection("reachouts").get().then(res=>{
-      setUser(res.docs.map(msg => msg.data()))
-    })
-    
+    db.collection("reachouts")
+      .get()
+      .then(res => {
+        setUser(res.docs.map(msg => msg.data()))
+      })
   }, [])
 
-  console.log('usr --- ',user)
-
-  return <p>Hello {user.length ? user.length : "there"}</p>
-};
+  return (
+    <p>
+      Hello{" "}
+      {user.length
+        ? user.map(data => (
+            <p>{`name: ${data.name}, email: ${data.email}, comment: ${data.message}`}</p>
+          ))
+        : "there"}
+    </p>
+  )
+}
 
 export default () => (
   <Layout>
     <LandingPage />
   </Layout>
-);
+)
