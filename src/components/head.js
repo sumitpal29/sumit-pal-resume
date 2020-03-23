c971b63585349ea89d8619f279fa3ab0ce552cc6
sumitpal29 <sumit@fusioncharts.com>
2020-03-23 23:22:11 +0530

    meta issues resolved

--- src/components/head.js
+++ src/components/head.js
@@ -2,5 +2,17 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 export default function head({ title }) {
-  return <Helmet title={title}></Helmet>
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+      <link rel="canonical" href="http://www.sumitpal.in" />
+      <meta name="description" content="Sumit Pal - resume - India based frontend web developer - available for hiring - nodejs developer" />
+      <meta name="keywords" content="Sumit Pal, resume, frontend, web developer, nodejs developer, javascript" />
+      <meta name="og:title" content="Sumit's resume" />
+      <meta name="og:description" content="Sumit Pal, India based frontend web developer. Checkout my profile here." />
+      <meta name="og:image" content="https://www.sumitpal.in/sumit-pal.jpg" />
+      <meta name="og:url" content="http://www.sumitpal.in"></meta>
+    </Helmet>
+  )
 }
